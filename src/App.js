cb7a610b4104267455c79cc717541effccd6614b
Ani Gokul <anigokul@Anis-MacBook-Air-2.local>
2024-07-16 15:06:20 +0530

    ui improvements

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>3D Point Cloud Viewer</h1>
-        <div className="dropdown-container">
-          <label htmlFor="view-select">View: </label>
-          <select id="view-select" value={view} onChange={handleViewChange}>
-            <option value="Free">Free</option>
-            <option value="XY">XY</option>
-            <option value="XZ">XZ</option>
-            <option value="YZ">YZ</option>
-          </select>
-        </div>
         <div className="canvas-container">
+          <div className="tools-container">
+            <label htmlFor="view-select">View: </label>
+            <select id="view-select" value={view} onChange={handleViewChange}>
+              <option value="Free">Free</option>
+              <option value="XY">Front</option>
+              <option value="XZ">Top-Down</option>
+              <option value="YZ">Side</option>
+            </select>
+          </div>
           <PointCloudViewer view={view} />
         </div>
       </header>
